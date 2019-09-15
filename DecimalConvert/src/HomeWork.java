955340a7558a0192d500613ae8e2c8e0bca97fa2
Aleksey <rezorqqq5@gmail.com>
2019-09-15 14:41:08 +0300

    createCalc

--- DecimalConvert/src/HomeWork.java
+++
@@ -1,5 +0,0 @@
-public class HomeWork {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ DecimalConvert/src/MainCalc.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MainCalc {
+    static char[] pool = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    static int userNumber;
+    static int a;
+
+    static String decimalConvert(int value, int system) {
+        String x = "";
+        while (value > 0) {
+            a = value % system;
+            x = pool[a] + "" + x;
+            value = value / system;
+        }
+        return x;
+    }
+
+    static void show() {
+        System.out.print("Binary system:" + decimalConvert(userNumber, 2) + "\n"
+                        + "Octal system:" + decimalConvert(userNumber, 8) + "\n"
+                        + "Hex system:" + decimalConvert(userNumber, 16));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        userNumber = scanner.nextInt();
+
+        show();
+    }
+}
