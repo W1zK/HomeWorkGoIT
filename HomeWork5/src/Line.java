80bcb23b1a52b41f521120c3598c635403033ee8
Aleksey <rezorqqq5@gmail.com>
2019-09-30 22:48:05 +0300

    1

--- HomeWork5/src/Line.java
+++ HomeWork5/src/Line.java
@@ -1,7 +1,11 @@
 class Line {
-    private double x;
+    private double a;
+    private double b;
+    private double c;
 
-    public Line(double x) {
-        this.x = x;
+    public Line(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 }
