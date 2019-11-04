f46f0e533778d3ea79a558878706905f4ad34a6f
Aleksey <rezorqqq5@gmail.com>
2019-11-04 13:45:34 +0200

    END++

--- Company/src/FilePath.java
+++ Company/src/FilePath.java
@@ -1,5 +1,5 @@
 public interface FilePath {
-    String FIXED_WORKER = "fixed.wrk";
-    String NON_FIXED_WORKER = "nonFixed.wrk";
-    String FREELANCE_WORKER = "freelance.wrk";
+    String FIXED_WORKER = "Files/fixed.wrk";
+    String NON_FIXED_WORKER = "Files/nonFixed.wrk";
+    String FREELANCE_WORKER = "Files/freelance.wrk";
 }
